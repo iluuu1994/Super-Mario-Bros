a639ef546356559886bf48db1ead03987e9dc702
Lukas Seglias <lukas_s94@Lukas-Segliass-MacBook-Pro.local>
2013-01-27 14:32:00 +0100

    Added TiledMapLayer

---
+++ Game/TiledMapScene.h
@@ -0,0 +1,16 @@
+//
+//  TiledMapScene.h
+//  Game
+//
+//  Created by Lukas Seglias on 27.01.13.
+//  Copyright 2013 Ilija Tovilo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface TiledMapScene : CCScene {
+    
+}
+
+@end
---
+++ Game/TiledMapScene.m
@@ -0,0 +1,22 @@
+//
+//  TiledMapScene.m
+//  Game
+//
+//  Created by Lukas Seglias on 27.01.13.
+//  Copyright 2013 Ilija Tovilo. All rights reserved.
+//
+
+#import "TiledMapScene.h"
+
+
+@implementation TiledMapScene
+
+-(id) init {
+    if (self = [super init]) {
+    
+        
+    }
+    return self;
+}
+
+@end
