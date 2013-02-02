5b4e15d24c0957e5318379b7eeacf69cec50b3ec
Ilija Tovilo <ilija.tovilo@me.com>
2013-02-02 17:25:47 +0100

    Fixed Candle logic

--- Game/Candle.h
+++ Game/Candle.h
@@ -13,6 +13,4 @@
     
 }
 
--(void)animate;
-
 @end
--- Game/Candle.m
+++ Game/Candle.m
@@ -13,33 +13,25 @@
 
 -(id)init {
     
-    if (self = [super init]) {
+    [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"Candle.plist"];
     
-        [self animate];
+    if (self = [super initWithSpriteFrameName:@"Candle_1.png"]) {
+        
+        NSMutableArray *animationFrames = [NSMutableArray array];
+        
+        for(int i = 1; i <= 4; ++i) {
+            [animationFrames addObject:[[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:[NSString stringWithFormat:@"Candle_%d.png", i]]];
+        }
+        
+        CCAnimation *animation = [CCAnimation animationWithSpriteFrames:animationFrames delay:0.1f];
+        CGSize winSize = [CCDirector sharedDirector].winSize;
+        
+        self.position = ccp(winSize.width/2, winSize.height/2);
+        CCAction *candleAction = [CCRepeatForever actionWithAction:[CCAnimate actionWithAnimation:animation]];
+        [self runAction:candleAction];
+        
     }
     return self;
-}
-
--(void) animate {
-    [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"Candle.plist"];
-    CCSpriteBatchNode *spriteSheet = [CCSpriteBatchNode batchNodeWithFile:@"Candle.png"];
-    
-    NSMutableArray *animationFrames = [NSMutableArray array];
-    
-    for(int i = 1; i <= 4; ++i) {
-        [animationFrames addObject:[[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:[NSString stringWithFormat:@"Candle_%d.png", i]]];
-    }
-    
-    CCAnimation *animation = [CCAnimation animationWithSpriteFrames:animationFrames delay:0.1f];
-    
-    CGSize winSize = [CCDirector sharedDirector].winSize;
-    CCSprite *candle = [CCSprite spriteWithSpriteFrameName:@"Candle_1.png"];
-    candle.position = ccp(winSize.width/2, winSize.height/2);
-    CCAction *candleAction = [CCRepeatForever actionWithAction:[CCAnimate actionWithAnimation:animation]];
-    [candle runAction:candleAction];
-    
-    [spriteSheet addChild:candle];
-    [self addChild:spriteSheet];
 }
 
 @end
