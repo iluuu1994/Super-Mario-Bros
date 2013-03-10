e3b475303fa9993fd46deae3ad7d9bcbaa5a80eb
Ilija Tovilo <ilija.tovilo@me.com>
2013-03-10 11:27:28 +0100

    Excluded docs from Project
    Docs were included in project
    They are know in a subfolder called Resouces/docs/

--- Game/HelloWorldLayer.m
+++ Game/HelloWorldLayer.m
@@ -22,24 +22,10 @@
 
 - (void)setUp {
     [super setUp];
-    
-    
+        
     [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"Candle.plist"];
     [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"Frei.plist"];
-    /*
-    CGSize winSize = [CCDirector sharedDirector].winSize;
-    
-    CCLayerColor* colorLayer = [CCLayerColor layerWithColor:ccc4(255, 255, 255, 255)];
-    [self addChild:colorLayer z:0];
-    
-    Candle *candle = [Candle node];
-    [candle setPosition:ccp(winSize.width / 3 * 2, winSize.height / 2)];
-    [self addChild:candle];
-    
-    
-    Frei *man = [Frei node];
-    [man setPosition:ccp(winSize.width / 3, winSize.height / 2)];
-    [self addChild:man];*/
+
     
     STTiledMapLayer *map = [STTiledMapLayer layerWithTiledMap:@"Level1.tmx"];
     [self addChild:map];
