98f369a88f39526bf75ee1ceb8c712586b4c253a
Lukas Seglias <lukas_s94@Lukas-Segliass-MacBook-Pro.local>
2013-04-10 20:11:34 +0200

    returned start scene to be level scene
    returned start scene to be level scene

--- Game/AppDelegate.m
+++ Game/AppDelegate.m
@@ -48,9 +48,7 @@
 	if(director.runningScene == nil) {
 		// Add the first scene to the stack. The director will draw it immediately into the framebuffer. (Animation is started automatically when the view is displayed.)
 		// and add the scene to the stack. The director will run it when it automatically when the view is displayed.
-		//[director runWithScene: [STLevelLayer layerWithWorldID:0 levelID:1]];
-        [director runWithScene: [STStartLayer node]];
-
+		[director runWithScene: [STLevelLayer layerWithWorldID:0 levelID:1]];
 	}
 }
 @end
