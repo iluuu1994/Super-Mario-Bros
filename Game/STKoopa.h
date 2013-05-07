6cbb2bf57e577525c222dec97e6d5b1f6b5b9713
Ilija Tovilo <ilija.tovilo@me.com>
2013-05-07 16:07:08 +0200

    Added Gumba and Koopa classes

---
+++ Game/STKoopa.h
@@ -0,0 +1,13 @@
+//
+//  STKoopa.h
+//  Game
+//
+//  Created by Ilija Tovilo on 5/7/13.
+//  Copyright (c) 2013 Ilija Tovilo. All rights reserved.
+//
+
+#import "STNPC.h"
+
+@interface STKoopa : STNPC
+
+@end
---
+++ Game/STNPC.m
@@ -0,0 +1,17 @@
+//
+//  STNPC.m
+//  Game
+//
+//  Created by Ilija Tovilo on 5/7/13.
+//  Copyright (c) 2013 Ilija Tovilo. All rights reserved.
+//
+
+#import "STNPC.h"
+
+@implementation STNPC
+
+- (STGameObjectBodyType)bodyType {
+    return STGameObjectBodyTypeDynamic;
+}
+
+@end
