893d836ee4810652e0fdd66e41267a6382a8fd41
Ilija Tovilo <ilija.tovilo@me.com>
2013-05-13 20:10:14 +0200

    Added Unit Tests Target

---
+++ Unit Tests/Unit_Tests.h
@@ -0,0 +1,13 @@
+//
+//  Unit_Tests.h
+//  Unit Tests
+//
+//  Created by Ilija Tovilo on 5/13/13.
+//  Copyright (c) 2013 Ilija Tovilo. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Unit_Tests : SenTestCase
+
+@end
---
+++ Unit Tests/Unit_Tests.m
@@ -0,0 +1,32 @@
+//
+//  Unit_Tests.m
+//  Unit Tests
+//
+//  Created by Ilija Tovilo on 5/13/13.
+//  Copyright (c) 2013 Ilija Tovilo. All rights reserved.
+//
+
+#import "Unit_Tests.h"
+
+@implementation Unit_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Unit Tests");
+}
+
+@end
